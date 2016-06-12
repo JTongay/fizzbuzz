be81b354f5ce9261e068bace972a5d2b922fec41
Joseph Tongay <dirk.tongay@gmail.com>
2016-06-12 17:42:18 -0500

    completed project

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(function () {
+  for (var i = 1; i <= 100; i++) {
+    console.log(i);
+    if (i % 3 === 0) {
+      console.log('fizz');
+      $('section').append('fizz').append('<br>');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+      $('section').append('buzz').append('<br>');
+    } else if ((i % 3 === 0) && (i % 5 === 0)) {
+      console.log('fizzbuzz');
+      $('section').append('fizzbuzz').append('<br>');
+    } else {
+      console.log(i);
+      $('section').append(i).append('<br>');
+    }
+  }
+
+
+});
